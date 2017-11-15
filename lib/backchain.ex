58e920e84f514a98ad6e2342ebcd2e249b3e52c6
Paul Tan <pjxtan@gmail.com>
2017-11-15 15:48:39 -0500

    Slightly smarter backchaining

--- lib/backchain.ex
+++ lib/backchain.ex
@@ -1,14 +1,11 @@
 defmodule Backchain do
+  import KB
+
   @doc """
   A (very) simple backchaining algorithm that does keep track of solutions
   or unify variables with constants.
   """
-  import KB
-  import Utils
-
-  def backchain1(kb, query) do
-    bc1(kb, [query])
-  end
+  def backchain1(kb, query), do: bc1(kb, [query])
 
   def bc1(kb, stack) do
     if Stack.empty?(stack) do
@@ -34,62 +31,41 @@ defmodule Backchain do
     end
   end
 
-  def test_antecedents(kb, stack, rule) do
-    antecedents = rule[:body]
-    stack = List.foldl(
-      antecedents,
-      stack,
-      fn(antecedent, stack) -> Stack.push(stack, antecedent) end
-    )
-    {stack, bc1(kb, stack)}
-  end
-
   @doc """
-  Attempts to unify two lists.
-  If successful, returns the unified list along with the variable resolutions.
-  Otherwise, returns :cannot_unify.
-  (Note: invalid variable or constant names should be already filtered out.)
+  A slightly smarter backchaining algorithm that unifies variables with constants,
+  but still does not keep track of solutions.
   """
-  def unify(list1, list2) do
-    if length(list1) != length(list2) do
-      :cannot_unify
+  def backchain2(kb, query), do: bc2(kb, [query])
+
+  def bc2(kb, stack) do
+    if Stack.empty?(stack) do
+      # Base case: all goals satisfied
+      true
     else
-      {unified, matches} = List.foldl(
-        List.zip([list1, list2]),
-        {[], []},
-        # TODO: Not loving all the if branching going on here...
-        fn({s1, s2}, {unified, matches}) ->
-          if starts_with_lowercase?(s1) do
-            if starts_with_lowercase?(s2) do
-              if s1 != s2 do
-                {[:cannot_unify | unified], matches}
-              else
-                # Unified two constants
-                {[s1 | unified], matches}
-              end
-            else
-              # Unified constant (s1) and variable (s2)
-              {[s1 | unified], [{s2, s1} | matches]}
-            end
-          else
-            if starts_with_uppercase?(s2) do
-              if s1 != s2 do
-                {[:cannot_unify | unified], matches}
-              else
-                # Unified two variables
-                {[s2 | unified], matches}
-              end
-            else
-              # Unified variable (s1) and constant (s2)
-              {[s2 | unified], [{s1, s2} | matches]}
-            end
-          end
-        end
-      )
-      if Enum.member?(unified, :cannot_unify) do
-        :cannot_unify
+      {goal, stack} = Stack.pop(stack)
+      if kb_fact?(kb, goal) do
+        # Current goal satisfied, test the rest
+        bc2(kb, stack)
       else
-        {Enum.reverse(unified), Enum.reverse(matches)}
+        # Go through matching rules and test their antecedents
+        results = List.foldl(
+          matching_rules(kb, goal),
+          [],
+          fn(rule, results) ->
+            {_, matches} = unify(rule[:head][:subjects], goal[:subjects])
+            # For each match, replace all instances of the variable with the constant
+            antecedents = List.foldl(
+              matches,
+              rule[:body],
+              fn({var, const}, antecedents) ->
+                replace_vars_with_const(antecedents, var, const)
+              end
+            )
+            stack = Stack.push_multiple(stack, antecedents)
+            [bc2(kb, stack) | results]
+          end
+        )
+        Enum.any?(results)
       end
     end
   end
--- test/frost_test.exs
+++ test/frost_test.exs
@@ -22,55 +22,83 @@ defmodule FrostTest do
     ]
   end
 
-  test "get matching rules from KB" do
+  test "get matching rules from KB with unification" do
     goal = KB.predicate("mortal", ["socrates"])
-    assert KB.matching_rules(kb1(), goal) == [
+    assert KB.matching_rules(kb2(), goal) == [
       KB.rule(
-        KB.predicate("mortal", ["socrates"]),
+        KB.predicate("mortal", ["X"]),
         [
-          KB.predicate("man", ["socrates"])
+          KB.predicate("man", ["X"])
         ]
       ),
       KB.rule(
-        KB.predicate("mortal", ["socrates"]),
+        KB.predicate("mortal", ["X"]),
         [
-          KB.predicate("woman", ["socrates"])
+          KB.predicate("woman", ["X"])
         ]
       )
     ]
   end
 
-  test "backchaining" do
+  test "replace variables with constants" do
+    predicates = [
+      KB.predicate("cool", ["X", "abc"]),
+      KB.predicate("awesome", ["def", "X"])
+    ]
+    assert KB.replace_vars_with_const(predicates, "X", "thing") == [
+      KB.predicate("cool", ["thing", "abc"]),
+      KB.predicate("awesome", ["def", "thing"])
+    ]
+
+  end
+
+  test "backchaining (no unification, no solutions)" do
     assert Backchain.backchain1(kb1(), KB.predicate("man", ["socrates"]))
-    assert not Backchain.backchain1(kb1(), KB.predicate("man", ["socrate"]))
+    refute Backchain.backchain1(kb1(), KB.predicate("woman", ["socrates"]))
+    refute Backchain.backchain1(kb1(), KB.predicate("man", ["socrate"]))
     assert Backchain.backchain1(kb1(), KB.predicate("mortal", ["socrates"]))
-    assert not Backchain.backchain1(kb1(), KB.predicate("mortal", ["socrate"]))
+    refute Backchain.backchain1(kb1(), KB.predicate("mortal", ["socrate"]))
+  end
+
+  test "backchaining (w/ unification, no solutions)" do
+    assert Backchain.backchain2(kb2(), KB.predicate("mortal", ["socrates"]))
+    assert Backchain.backchain2(kb2(), KB.predicate("mortal", ["beauvoir"]))
+    #refute Backchain.backchain2(kb2(), KB.predicate("man", ["socrate"]))
+    #assert Backchain.backchain2(kb2(), KB.predicate("mortal", ["socrates"]))
+    #refute Backchain.backchain2(kb2(), KB.predicate("mortal", ["socrate"]))
+  end
+
+  test "backchaining (w/ unification, w/ solutions)" do
+    :todo
   end
 
   test "unify lists" do
-    assert Backchain.unify(["a", "b", "c"], ["a", "b", "c"]) ==
+    assert KB.can_unify?(["a", "Y", "c"], ["X", "b", "Z"])
+    refute KB.can_unify?(["W", "Y", "c"], ["X", "b", "Z"])
+
+    assert KB.unify(["a", "b", "c"], ["a", "b", "c"]) ==
       {["a", "b", "c"], []}
-    assert Backchain.unify(["a", "b", "c"], ["a", "b", "d"]) ==
+    assert KB.unify(["a", "b", "c"], ["a", "b", "d"]) ==
       :cannot_unify
-    assert Backchain.unify(["a", "b", "c"], ["a", "b", "X"]) ==
+    assert KB.unify(["a", "b", "c"], ["a", "b", "X"]) ==
       {["a", "b", "c"], [{"X", "c"}]}
-    assert Backchain.unify(["a", "b", "Y"], ["a", "b", "c"]) ==
+    assert KB.unify(["a", "b", "Y"], ["a", "b", "c"]) ==
       {["a", "b", "c"], [{"Y", "c"}]}
-    assert Backchain.unify(["a", "b", "X"], ["a", "b", "X"]) ==
+    assert KB.unify(["a", "b", "X"], ["a", "b", "X"]) ==
       {["a", "b", "X"], []}
-    assert Backchain.unify(["a", "b", "X"], ["a", "b", "Y"]) ==
+    assert KB.unify(["a", "b", "X"], ["a", "b", "Y"]) ==
       :cannot_unify
-    assert Backchain.unify(["Z", "b", "X"], ["a", "Y", "c"]) ==
+    assert KB.unify(["Z", "b", "X"], ["a", "Y", "c"]) ==
       {["a", "b", "c"], [{"Z", "a"}, {"Y", "b"}, {"X", "c"}]}
   end
 
   test "string utils" do
     assert Utils.valid_var_or_const?("socrates1")
-    assert not Utils.valid_var_or_const?("1socrates1")
+    refute Utils.valid_var_or_const?("1socrates1")
     assert Utils.starts_with_uppercase?("Socrates")
-    assert not Utils.starts_with_uppercase?("socrates")
+    refute Utils.starts_with_uppercase?("socrates")
     assert Utils.starts_with_lowercase?("socrates")
-    assert not Utils.starts_with_lowercase?("Socrates")
+    refute Utils.starts_with_lowercase?("Socrates")
   end
 
   def kb1() do
@@ -88,6 +116,29 @@ defmodule FrostTest do
         KB.predicate("mortal", ["socrates"]),
         [
           KB.predicate("woman", ["socrates"])
+        ]
+      )
+    ]
+  end
+
+  def kb2() do
+    [
+      KB.rule(
+        KB.predicate("man", ["socrates"])
+      ),
+      KB.rule(
+        KB.predicate("woman", ["beauvoir"])
+      ),
+      KB.rule(
+        KB.predicate("mortal", ["X"]),
+        [
+          KB.predicate("man", ["X"])
+        ]
+      ),
+      KB.rule(
+        KB.predicate("mortal", ["X"]),
+        [
+          KB.predicate("woman", ["X"])
         ]
       )
     ]

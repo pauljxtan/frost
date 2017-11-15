d6b51129d815cc77974bd6efa1736c5dae99fc73
Paul Tan <pjxtan@gmail.com>
2017-11-15 13:58:38 -0500

    Added tests

--- test/frost_test.exs
+++ test/frost_test.exs
@@ -2,7 +2,37 @@ defmodule FrostTest do
   use ExUnit.Case
   doctest Frost
 
-  test "greets the world" do
-    assert Frost.hello() == :world
+  test "get facts from KB" do
+    assert KB.facts(kb1()) == [
+      KB.rule(
+        KB.predicate("man", ["socrates"])
+      )
+    ]
+  end
+
+  test "stack operations" do
+    stack = Stack.new()
+    assert Stack.empty?(stack)
+    stack = stack |> Stack.push(1)
+    assert Stack.peek(stack) == 1
+    stack = stack |> Stack.push(2) |> Stack.push(3)
+    assert Stack.peek(stack) == 3
+    {top, stack} = Stack.pop(stack)
+    assert Stack.peek(stack) == 2
+    assert top == 3
+  end
+
+  def kb1() do
+    [
+      KB.rule(
+        KB.predicate("man", ["socrates"])
+      ),
+      KB.rule(
+        KB.predicate("mortal", ["socrates"]),
+        [
+          KB.predicate("man", ["socrates"])
+        ]
+      )
+    ]
   end
 end
